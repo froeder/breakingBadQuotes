519d2028cb3c024bc8c846a81c42012f45df5690
Jhonatan Froeder <froeder3@gmail.com>
2018-06-28 20:42:40 -0400

    Incluido axios

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,34 +2,33 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
+        <div>
+          <p>{{this.quote}}</p>
+          <small>{{this.author}}</small>
+        </div>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      quote: '',
+      author: ''
+    }
+  },
+  mounted (){
+    let uri = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes/1'
+    axios.get(uri).then((response) => {
+      console.log(response.data[0])
+      this.quote = response.data[0].quote
+      this.author = response.data[0].author
+    })
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+</script>
+
